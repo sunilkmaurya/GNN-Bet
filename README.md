910e3b84b9e4c360b8bffa3b288ba2f99082114d
Sunil Kumar Maurya <skmaurya@net.c.titech.ac.jp>
2019-08-26 17:42:09 +0900

    upload_code
    Uploading code to github.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Code repository for GNN-Bet
+This repository contains implementation for paper "Fast approximations of betweenness centrality using Graph Neural Networks".
+
+Code is written in Python and the proposed model is implemented using Pytorch.
+
+**Main package Requirements**: torch, networkit, networkx and scipy.
+Use of conda environment is recommended but not necessary.
+
+For easy check, we have provided small synthetic dataset. The given dataset consists of 30 scalefree graphs with number of nodes varying from 1000 to 10,000. Graphs are stored as networkx graphs and betweenness centrality values are stored in dictionaries. 20 graphs are used for training and 10 graphs for testing. 20 adjacency matrices are created from each training graph by randomly permutating the node sequences. Hence we have total 400 samples for training.
+
+**Running the code**:
+
+    python main.py
+
+outputs the average Kendall's Tau score of test graphs.
---
+++ main.py
@@ -0,0 +1,99 @@
+ 
+import numpy as np
+import pickle
+import networkx as nx
+import torch
+from utils import *
+import random
+import torch.nn as nn
+from model import GNN_Bet
+torch.manual_seed(20)
+
+#Loading graph data
+
+data_path = "./data/"
+
+#Load training data
+print(f"Loading data...")
+with open(data_path+"training.pickle","rb") as fopen:
+    list_graph_train,list_n_seq_train,list_num_node_train,bc_mat_train = pickle.load(fopen)
+
+
+with open(data_path+"test.pickle","rb") as fopen:
+    list_graph_test,list_n_seq_test,list_num_node_test,bc_mat_test = pickle.load(fopen)
+
+model_size = 10000
+#Get adjacency matrices from graphs
+print(f"Graphs to adjacency conversion.")
+
+list_adj_train,list_adj_t_train = graph_to_adj(list_graph_train,list_n_seq_train,list_num_node_train,model_size)
+list_adj_test,list_adj_t_test = graph_to_adj(list_graph_test,list_n_seq_test,list_num_node_test,model_size)
+
+
+
+def train(list_adj_train,list_adj_t_train,list_num_node_train,bc_mat_train):
+    model.train()
+    total_count_train = list()
+    loss_train = 0
+    num_samples_train = len(list_adj_test)
+    for i in range(num_samples_train):
+        adj = list_adj_train[i]
+        num_nodes = list_num_node_train[i]
+        adj_t = list_adj_t_train[i]
+        adj = adj.to(device)
+        adj_t = adj_t.to(device)
+
+        optimizer.zero_grad()
+            
+        y_out = model(adj,adj_t)
+        true_arr = torch.from_numpy(bc_mat_train[:,i]).float()
+        true_val = true_arr.to(device)
+        
+        loss_rank = loss_cal(y_out,true_val,num_nodes,device,model_size)
+        loss_train = loss_train + float(loss_rank)
+        loss_rank.backward()
+        optimizer.step()
+
+def test(list_adj_test,list_adj_t_test,list_num_node_test,bc_mat_test):
+    model.eval()
+    loss_val = 0
+    list_kt = list()
+    num_samples_test = len(list_adj_test)
+    for j in range(num_samples_test):
+        adj = list_adj_test[j]
+        adj_t = list_adj_t_test[j]
+        adj=adj.to(device)
+        adj_t = adj_t.to(device)
+        num_nodes = list_num_node_test[j]
+        
+        y_out = model(adj,adj_t)
+    
+        
+        true_arr = torch.from_numpy(bc_mat_test[:,j]).float()
+        true_val = true_arr.to(device)
+    
+        kt = ranking_correlation(y_out,true_val,num_nodes,model_size)
+        list_kt.append(kt)
+
+    print(f"Average kendall tau score is: {np.mean(np.array(list_kt))} and std: {np.std(np.array(list_kt))}")
+
+
+
+#Model parameters
+hidden = 20
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = GNN_Bet(ninput=model_size,nhid=hidden,dropout=0.6)
+model.to(device)
+optimizer = torch.optim.Adam(model.parameters(),lr=0.0005)
+num_epoch = 10
+
+print(f"Number of epoches: {num_epoch}")
+for e in range(num_epoch):
+    print(f"Epoch number: {e}")
+    train(list_adj_train,list_adj_t_train,list_num_node_train,bc_mat_train)
+#test on 10 test graphs and print average KT Score and its stanard deviation
+test(list_adj_test,list_adj_t_test,list_num_node_test,bc_mat_test)
+
+
+    
